0c83a22680b819a87811975c5600dcce30a5f376
Administrátor12 (ADM 12) <admin12@mb-net.cz>
2017-08-13 16:08:27 +0200

    Přidejte soubory projektu.

---
+++ GcodeConverter/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace GcodeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofDlg = new System.Windows.Forms.OpenFileDialog();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.laserON = new System.Windows.Forms.TextBox();
+            this.laserOFF = new System.Windows.Forms.TextBox();
+            this.pwmEnabled = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.starttime = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.starttime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(29, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(621, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose LaserWEB Gcode files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ofDlg
+            // 
+            this.ofDlg.FileName = "*.gcode";
+            // 
+            // lb
+            // 
+            this.lb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb.FormattingEnabled = true;
+            this.lb.Location = new System.Drawing.Point(29, 90);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(621, 82);
+            this.lb.TabIndex = 1;
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(29, 200);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(285, 35);
+            this.convert.TabIndex = 2;
+            this.convert.Text = "Convert to Marlin Gcode";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Laser ON command:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Laser OFF command:";
+            // 
+            // laserON
+            // 
+            this.laserON.Location = new System.Drawing.Point(447, 193);
+            this.laserON.Name = "laserON";
+            this.laserON.Size = new System.Drawing.Size(63, 20);
+            this.laserON.TabIndex = 4;
+            this.laserON.Text = "M106";
+            // 
+            // laserOFF
+            // 
+            this.laserOFF.Location = new System.Drawing.Point(447, 220);
+            this.laserOFF.Name = "laserOFF";
+            this.laserOFF.Size = new System.Drawing.Size(63, 20);
+            this.laserOFF.TabIndex = 4;
+            this.laserOFF.Text = "M107";
+            this.laserOFF.TextChanged += new System.EventHandler(this.laserOFF_TextChanged);
+            // 
+            // pwmEnabled
+            // 
+            this.pwmEnabled.AutoSize = true;
+            this.pwmEnabled.Checked = true;
+            this.pwmEnabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.pwmEnabled.Location = new System.Drawing.Point(535, 192);
+            this.pwmEnabled.Name = "pwmEnabled";
+            this.pwmEnabled.Size = new System.Drawing.Size(129, 17);
+            this.pwmEnabled.TabIndex = 5;
+            this.pwmEnabled.Text = "PWM control enabled";
+            this.pwmEnabled.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Input Files";
+            // 
+            // lb2
+            // 
+            this.lb2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb2.FormattingEnabled = true;
+            this.lb2.Location = new System.Drawing.Point(29, 286);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(621, 82);
+            this.lb2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Output Files";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Laser start time [ms]:";
+            // 
+            // starttime
+            // 
+            this.starttime.Location = new System.Drawing.Point(447, 250);
+            this.starttime.Name = "starttime";
+            this.starttime.Size = new System.Drawing.Size(63, 20);
+            this.starttime.TabIndex = 10;
+            this.starttime.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 414);
+            this.Controls.Add(this.starttime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pwmEnabled);
+            this.Controls.Add(this.laserOFF);
+            this.Controls.Add(this.laserON);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(690, 450);
+            this.Name = "Form1";
+            this.Text = "LaserWEB Gcode to Marlin Gcode";
+            ((System.ComponentModel.ISupportInitialize)(this.starttime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofDlg;
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox laserON;
+        private System.Windows.Forms.TextBox laserOFF;
+        private System.Windows.Forms.CheckBox pwmEnabled;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lb2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown starttime;
+    }
+}
+
---
+++ GcodeConverter/Form1.cs
@@ -0,0 +1,242 @@
+﻿/**
+ * Gcode converter
+ * Copyright (C) 2017 Milan Peksa
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GcodeConverter
+{
+    public partial class Form1 : Form
+    {
+        string[] fns;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb.Items.Clear();
+            lb2.Items.Clear();
+            ofDlg.Multiselect = true;
+            var dr=ofDlg.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                fns = ofDlg.FileNames;
+                foreach (string s in fns) lb.Items.Add(s);
+            }
+            else
+                fns = null;
+        }
+
+        private void laserOFF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            string lineOfText;
+            string LastCmd = "";
+            StringBuilder os=new StringBuilder();
+            foreach (string f in fns)
+            {
+                
+                    string of = System.IO.Path.GetDirectoryName(f) + "\\" + System.IO.Path.GetFileNameWithoutExtension(f) + ".marlin" + System.IO.Path.GetExtension(f);
+                    System.IO.FileStream fs,fsw;
+                    using (fs = new System.IO.FileStream(f, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                    {
+                        using (fsw = new System.IO.FileStream(of, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+                        {
+                            double lastspeed = 9999;
+                           
+                            var file = new System.IO.StreamReader(fs, System.Text.Encoding.UTF8, true);
+                            var fileW = new System.IO.StreamWriter(fsw, System.Text.Encoding.UTF8);
+                            while ((lineOfText = file.ReadLine()) != null)
+                            { 
+                                restart:
+                                bool comment = false;
+                                bool cmdflag = false;
+                                bool speedflag = false;
+                                os.Clear();
+                                double speed=0.0;
+                                string comapp = "";
+                                var rc= lineOfText.Trim().Split(';')[0];
+                                if (lineOfText.Trim().Split(';').Length>1)// save comment 
+                                {
+                                    for (int k=1;k<lineOfText.Trim().Split(';').Length;k++)
+                                    {
+                                    comapp +=";" + lineOfText.Trim().Split(';')[k];
+                                    }
+                                }
+                                var ss = rc.Trim().Split(' ');
+                                if (ss[0].Length > 0)
+                                {
+                                    
+                                    if (ss[0][0] == 'G') { LastCmd = ss[0]; cmdflag = true; }
+                                    if (ss[0][0] == 'X' && ss.Length < 2 && !cmdflag) 
+                                    {
+                                        repeatreading:
+                                        String tmp = file.ReadLine();
+                                        if (tmp!= null && tmp.Trim().Length>2)
+                                        {
+
+                                            if (tmp[0] == 'X' && ss.Length < 2)
+                                            {
+                                            lineOfText = tmp;
+                                            goto repeatreading;
+
+                                            }
+                                            else
+                                            {
+                                            fileW.WriteLine(LastCmd + ' ' + lineOfText);
+                                            lineOfText = tmp;
+                                            goto restart;
+                                            }
+                                            
+
+                                        }
+                                        else
+                                        fileW.WriteLine(LastCmd + ' ' + lineOfText);
+                                        lineOfText = "  "; 
+                                        goto restart;
+                                    }
+                                if (ss[0][0] == 'Y' && ss.Length < 2 && !cmdflag)
+                                {
+                                repeatreading:
+                                    String tmp = file.ReadLine();
+                                    if (tmp != null && tmp.Trim().Length > 2)
+                                    {
+
+                                        if (tmp[0] == 'Y' && ss.Length < 2)
+                                        {
+                                            lineOfText = tmp;
+                                            goto repeatreading;
+
+                                        }
+                                        else
+                                        {
+                                            fileW.WriteLine(LastCmd + ' ' + lineOfText);
+                                            lineOfText = tmp;
+                                            goto restart;
+                                        }
+
+
+                                    }
+                                    else
+                                        fileW.WriteLine(LastCmd + ' ' + lineOfText);
+                                    lineOfText = "  ";
+                                    goto restart;
+                                }
+
+
+                                if ( ss[0].ToUpper()[0] != 'M') // only G and M  commands are alowed for proper function
+                                    {
+                                        if (!cmdflag) os.Append(LastCmd + ' ');
+                                        foreach (string ps in ss)
+                                        {
+                                        
+                                            if (ps.Length == 0)
+                                                os.Append(" ");
+                                            else
+                                            {
+                                                if (ps[0] == ';') comment = true;
+                                                if (ps.ToUpper()[0] != 'S' || comment)
+                                                {
+                                                    os.Append(ps + " ");
+                                                }
+                                                else
+                                                {  // S attribut
+                                                    speedflag = true;
+                                                    bool rt = Double.TryParse(ps.Substring(1), System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.CreateSpecificCulture("en-US"), out speed);
+                                                    if (!rt) speed = lastspeed;
+
+                                                }
+                                            }
+                                        }
+
+                                        if (lastspeed != speed && speedflag)
+                                        {
+                                        if (speed > 0)
+                                        { 
+                                            if (pwmEnabled.Checked)
+                                                fileW.WriteLine(laserON.Text + " S" + ((int)(speed * 255.0)).ToString());
+                                            else
+                                                fileW.WriteLine(laserON.Text);
+                                            if (starttime.Value>0 && speed>0.5) fileW.WriteLine("G4 P" + starttime.Value.ToString());
+                                        }
+                                        else
+                                        {
+
+                                            fileW.WriteLine(laserOFF.Text);
+                                        }
+                                            lastspeed = speed;
+                                        }
+                                        else
+                                        {
+                                        if (LastCmd.Contains("G0") && lastspeed == 1)// for G0 commands without S attribute
+                                        {
+                                            fileW.WriteLine(laserOFF.Text);
+                                            lastspeed = 0;
+                                        }
+                                        if (LastCmd.Contains("G1") && lastspeed == 0)// for G1 commands without S attribute
+                                        {
+                                            fileW.WriteLine(laserON.Text + " S255");
+                                            if (starttime.Value > 0 && speed > 0.5) fileW.WriteLine("G4 P" + starttime.Value.ToString());
+                                            lastspeed = 1;
+                                        }
+                                    }
+                                
+
+                                    }
+                                    else
+                                        os.Append(rc);
+                                }
+                                fileW.WriteLine(os + comapp);
+                               
+                            }
+                            fileW.WriteLine(laserOFF.Text);
+                            fileW.Flush();
+
+                        }
+                    }
+
+                lb2.Items.Add(of);
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
